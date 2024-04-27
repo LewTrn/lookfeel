692e108096f7b40bb236262c44124dd661ffa0e9
LewTrn <lewistran96@gmail.com>
2024-04-27 17:56:49 +0100

    chore: add typography component

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,42 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+
+import { cn } from "~/lib/utils";
+
+export const variants = cva("select-none", {
+  variants: {
+    variant: {
+      h1: "mb-4 text-4xl font-bold",
+      h2: "mb-2 text-xl font-semibold",
+      h3: "mb-1 text-lg font-semibold",
+      h4: "text-sm font-semibold",
+      body: "text-md",
+      caption: "text-xs",
+    },
+  },
+  defaultVariants: {
+    variant: "body",
+  },
+});
+
+export interface TypographyProps
+  extends React.HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof variants> {
+  as?: React.ElementType;
+}
+
+const Typography = React.forwardRef<HTMLHeadingElement, TypographyProps>(
+  ({ className, variant, as, ...props }, ref) => {
+    const Comp = as ?? "span";
+    return (
+      <Comp
+        className={cn(variants({ variant, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Typography.displayName = "Typography";
+
+export default Typography;
