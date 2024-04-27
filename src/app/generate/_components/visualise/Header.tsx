a3ec89f1110e468cb02e6daab8ccc19d09245664
LewTrn <lewistran96@gmail.com>
2024-04-27 17:26:03 +0100

    feat: add visualise header buttons

--- src/app/generate/_components/visualise/Header.tsx
+++ src/app/generate/_components/visualise/Header.tsx
@@ -1,10 +1,15 @@
+import { RedoIcon, UndoIcon } from "lucide-react";
+
 import { Button } from "~/components/ui/button";
+import { IconButton } from "~/components/ui/icon-button";
 import { strings } from "~/locales/generate";
 
 export const Header = () => {
   return (
     <div className="mb-4 flex h-16 items-center justify-between">
-      <div>
+      <div className="flex items-center gap-2">
+        <IconButton Icon={UndoIcon} />
+        <IconButton Icon={RedoIcon} />
         <Button variant="ghost">
           {strings.visualise.header.generate.action}
         </Button>
