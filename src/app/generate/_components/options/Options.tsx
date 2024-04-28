abedfe36ea909eedc7853f5cdaea8bc5059c2817
LewTrn <lewistran96@gmail.com>
2024-04-28 06:37:40 +0100

    feat: add placeholder template select

--- src/app/generate/_components/options/Options.tsx
+++ src/app/generate/_components/options/Options.tsx
@@ -1,3 +1,7 @@
+import { ChevronDownIcon } from "lucide-react";
+
+import Typography from "~/components/ui/typography";
+
 import { Fonts } from "./Fonts";
 import { Header } from "./Header";
 import { Palette } from "./Palette";
@@ -6,7 +10,13 @@ export const Options = () => {
   return (
     <div>
       <Header />
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between rounded-lg bg-accent px-4 py-2">
+          <Typography variant="h3" className="mb-0">
+            Style Tile
+          </Typography>
+          <ChevronDownIcon height={20} width={20} />
+        </div>
         <Palette />
         <Fonts />
       </div>
