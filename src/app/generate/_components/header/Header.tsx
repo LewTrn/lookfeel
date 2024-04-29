75c709764cdb76189e525bb4498e319d8b5056e2
LewTrn <lewistran96@gmail.com>
2024-04-29 21:50:15 +0100

    feat: add generate on click and space

--- src/app/generate/_components/header/Header.tsx
+++ src/app/generate/_components/header/Header.tsx
@@ -1,11 +1,32 @@
+"use client";
+
 import { RedoIcon, UndoIcon } from "lucide-react";
 import Link from "next/link";
+import { useEffect } from "react";
 
 import { Button } from "~/components/ui/button";
 import { IconButton } from "~/components/ui/icon-button";
 import { strings } from "~/locales/generate";
 
+import { useGenerateStore } from "../../_store/useGenerateStore";
+
 export const Header = () => {
+  const generatePalette = useGenerateStore((state) => state.generatePalette);
+
+  useEffect(() => {
+    const onSpace = (event: KeyboardEvent) => {
+      if (event.code === "Space") {
+        generatePalette();
+      }
+    };
+
+    document.addEventListener("keydown", onSpace);
+
+    return () => {
+      document.removeEventListener("keydown", onSpace);
+    };
+  }, [generatePalette]);
+
   return (
     <header className="mx-2 mb-4 flex h-16 items-center justify-between gap-8">
       <Link href="/" className="w-[19.5rem] text-2xl font-bold">
@@ -15,7 +36,7 @@ export const Header = () => {
         <div className="flex items-center gap-2">
           <IconButton Icon={UndoIcon} variant="ghost" />
           <IconButton Icon={RedoIcon} variant="ghost" />
-          <Button variant="ghost">
+          <Button variant="ghost" onClick={() => generatePalette()}>
             {strings.visualise.header.generate.action}
           </Button>
         </div>
