0e033463d61a00c50a30afd83a76308929fcfa70
LewTrn <lewistran96@gmail.com>
2024-05-01 21:09:24 +0100

    feat: add shuffle util

---
+++ src/utils/shuffle.ts
@@ -0,0 +1,8 @@
+export const shuffle = <T>(array: T[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j] as T, array[i] as T];
+  }
+
+  return array;
+};
