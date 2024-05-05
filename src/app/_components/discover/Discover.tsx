2cfd1ba4d727b07771176632172895b6eee7270a
LewTrn <lewistran96@gmail.com>
2024-05-05 23:43:21 +0100

    feat: add example theme card

--- src/app/_components/discover/Discover.tsx
+++ src/app/_components/discover/Discover.tsx
@@ -5,25 +5,39 @@ import { FlameIcon, SparklesIcon, TrendingUpIcon } from "lucide-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { strings } from "~/locales/landing";
 
+import { ThemeCard } from "./ThemeCard";
+
 export const Discover = () => {
   return (
-    <div>
-      <Tabs defaultValue="trending">
-        <TabsList className="mb-6">
-          <TabsTrigger value="trending" Icon={FlameIcon}>
-            {strings.discover.filter.trending.action}
-          </TabsTrigger>
-          <TabsTrigger value="popular" Icon={TrendingUpIcon}>
-            {strings.discover.filter.popular.action}
-          </TabsTrigger>
-          <TabsTrigger value="latest" Icon={SparklesIcon}>
-            {strings.discover.filter.latest.action}
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="trending"></TabsContent>
-        <TabsContent value="popular"></TabsContent>
-        <TabsContent value="latest"></TabsContent>
-      </Tabs>
-    </div>
+    <Tabs defaultValue="trending">
+      <TabsList className="mb-6">
+        <TabsTrigger value="trending" Icon={FlameIcon}>
+          {strings.discover.filter.trending.action}
+        </TabsTrigger>
+        <TabsTrigger value="popular" Icon={TrendingUpIcon}>
+          {strings.discover.filter.popular.action}
+        </TabsTrigger>
+        <TabsTrigger value="latest" Icon={SparklesIcon}>
+          {strings.discover.filter.latest.action}
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="trending">
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <ThemeCard
+            headingFont="Noto Sans Japanese"
+            bodyFont="Noto Sans Japanese"
+          />
+          <ThemeCard headingFont="Noto Sans" bodyFont="Georgia" />
+          <ThemeCard
+            headingFont="Noto Sans Japanese"
+            bodyFont="Noto Sans Japanese"
+          />
+          <ThemeCard headingFont="Noto Sans" bodyFont="Georgia" />
+          <ThemeCard headingFont="Noto Sans" bodyFont="Georgia" />
+        </div>
+      </TabsContent>
+      <TabsContent value="popular"></TabsContent>
+      <TabsContent value="latest"></TabsContent>
+    </Tabs>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Hero } from "./_components/Hero";
 
 export default async function Home() {
   return (
-    <main className="mx-auto w-full max-w-screen-2xl px-8">
+    <main className="mx-auto w-full max-w-screen-2xl px-8 pb-8">
       <Header />
       <div className="flex flex-col gap-8">
         <Hero />
