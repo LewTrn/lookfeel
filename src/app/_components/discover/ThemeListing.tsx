58fd8c55b943e3670c1f474f20126e0077abd22b
LewTrn <lewistran96@gmail.com>
2024-05-18 15:53:43 +0100

    feat: add skeleton and hide scrollbars

--- src/app/_components/discover/ThemeListing.tsx
+++ src/app/_components/discover/ThemeListing.tsx
@@ -8,14 +8,26 @@ type ThemeListingProps = {
   filter: "latest" | "trending";
 };
 
+const CardSkeleton = () => (
+  <div className="h-[182.5px] animate-pulse rounded-lg bg-accent/50" />
+);
+
 export const ThemeListing = ({ filter }: ThemeListingProps) => {
-  const { data } = api.theme.getThemes.useQuery(
+  const { data, isLoading } = api.theme.getThemes.useQuery(
     { filter },
     { refetchOnMount: false },
   );
 
   return (
     <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {isLoading && (
+        <>
+          <CardSkeleton />
+          <CardSkeleton />
+          <CardSkeleton />
+          <CardSkeleton />
+        </>
+      )}
       {data?.map(({ id, liked, like_count: likeCount = 0, ...rest }) => (
         <DiscoverThemeCard
           key={id}
--- src/components/theme/view/details/ThemeDetails.tsx
+++ src/components/theme/view/details/ThemeDetails.tsx
@@ -9,7 +9,7 @@ type ThemeDetailsProps = {
 
 export const ThemeDetails = ({ theme }: ThemeDetailsProps) => {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="sticky top-20 flex flex-col gap-6">
       <PaletteDetails palette={theme.palette} />
       <FontDetails fonts={theme.fonts} />
     </div>
