35e284ba7c8253cd452c72adfda45ee93f15bddc
LewTrn <lewistran96@gmail.com>
2024-05-19 20:49:59 +0100

    feat: add eyedropper component

---
+++ src/app/generate/_components/colours/EyeDropperButton.tsx
@@ -0,0 +1,39 @@
+import { PipetteIcon } from "lucide-react";
+
+import { IconButton } from "~/components/ui/icon-button";
+
+declare class EyeDropper {
+  open: () => Promise<{ sRGBHex: string }>;
+}
+
+type EyeDropperButton = {
+  onSelectColour: (colour: string) => void;
+  onClose: () => void;
+};
+
+export const EyeDropperButton = ({
+  onSelectColour,
+  onClose,
+}: EyeDropperButton) => {
+  const isSupported = "EyeDropper" in window;
+
+  if (!isSupported) return null;
+
+  const handleSelectColour = () => {
+    const dropper = new EyeDropper();
+
+    dropper
+      .open()
+      .then(({ sRGBHex }) => {
+        onSelectColour(sRGBHex);
+        onClose();
+      })
+      .catch(() => {
+        onClose();
+      });
+  };
+
+  return (
+    <IconButton Icon={PipetteIcon} size="sm" onClick={handleSelectColour} />
+  );
+};
